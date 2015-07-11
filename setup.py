732448e65190794a02458ab387626185662d684d
straup <github@aaronland.net>
2015-07-10 22:28:15 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ packages = find_packages()
 setup(
     name='flamework.api',
     namespace_packages=['flamework', 'flamework.api', 'flamework.api.client', 'flamework.api.request'],
-    version='0.1',
+    version='0.2',
     description='Simple Python wrapper for Flamework derived APIs',
-    author='Smithsonian Cooper-Hewitt National Design Museum',
-    url='https://github.com/cooperhewitt/py-flamework-api',
+    author='Aaron Straup Cope',
+    url='https://github.com/straup/py-flamework-api',
     install_requires=[
         'requests',
         ],
     packages=packages,
     scripts=[],
-    download_url='https://github.com/cooperhewitt/py-flamework-api/releases/tag/v0.1',
+    download_url='https://github.com/straup/py-flamework-api/releases/tag/v0.2',
     license='BSD')
